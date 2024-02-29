44f77cc4ede5e179dcb43a6991d6e56fa21a1463
sonofthechallenger <161762437+sonofthechallenger@users.noreply.github.com>
2024-02-29 16:11:50 -0600

    add index script and styles

---
+++ script.js
@@ -0,0 +1,28 @@
+//DRAGGABLE IMAGE
+var draggable = document.getElementById('draggable');
+
+var posX = 0,
+    posY = 0,
+    mouseX = 0,
+    mouseY = 0;
+
+draggable.addEventListener('mousedown', mouseDown, false);
+window.addEventListener('mouseup', mouseUp, false);
+
+function mouseDown(e) {
+  e.preventDefault();
+  posX = e.clientX - draggable.offsetLeft;
+  posY = e.clientY - draggable.offsetTop;
+  window.addEventListener('mousemove', moveElement, false);
+}
+
+function mouseUp() {
+  window.removeEventListener('mousemove', moveElement, false);
+}
+
+function moveElement(e) {
+  mouseX = e.clientX - posX;
+  mouseY = e.clientY - posY;
+  draggable.style.left = mouseX + 'px';
+  draggable.style.top = mouseY + 'px';
+}
